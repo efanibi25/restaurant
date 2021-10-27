56320a5e5e6d29a8313f4f5d1f35783ec5278b06
john <you@example.com>
2021-10-27 17:48:08 -0500

    initiail commit react

---
+++ src/Pages/Waitlist.js
@@ -0,0 +1,20 @@
+import Row from "../Table/Row";
+function Waitlist() {
+    //fetch table from backend, map each as seperate rows
+    return (
+        <table>
+        <thead>
+        </thead>
+        <tbody>
+        <Row>
+        </Row>
+        </tbody>
+ 
+        </table>
+       
+        
+    );
+  }
+  
+  export default Waitlist;
+  
---
+++ src/Table/Row.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react/cjs/react.production.min";
+function Row() {
+    return (
+        <Fragment>
+                    <td>
+          sssssssss  
+            </td>
+          <td>
+          sssssssss  
+            </td>
+
+<td>
+sssssssss  
+  </td>
+        </Fragment>
+
+    );
+  }
+  
+  export default Row;
+  
