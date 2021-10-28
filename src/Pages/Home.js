ed469d0bd4741f927c5eb27cf32b6dae3464dcb5
john <you@example.com>
2021-10-28 15:02:23 -0500

    added filter

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,17 @@
 
+import { Fragment } from "react";
 import MenuBar from "../Menubar/Menubar";
 function Home() {
     return(
 
+
+<Fragment>
 <MenuBar/>
-    
+<h1>This is a home Page</h1>
+<p></p>
+<h1>Checkout Those Links</h1>
+
+</Fragment>
     )
     
   }
