79740e308bd56692b516cf85d8800391ff1886e3
Charlie <cleungtech@outlook.com>
2021-11-07 17:26:09 -0500

    Added Drawer to the Menubar

--- src/Component/Menubar.js
+++ src/Component/Menubar.js
@@ -5,12 +5,15 @@ import {
     CssBaseline,
     Typography,
     makeStyles,
+    useTheme,
+    useMediaQuery,
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import DrawerComponent from "./Drawer";
 
 const useStyles = makeStyles((theme) => ({
     navlinks: {
-        marginLeft: theme.spacing(10),
+        marginLeft: theme.spacing(5),
         display: "flex",
     },
     logo: {
@@ -32,6 +35,8 @@ const useStyles = makeStyles((theme) => ({
 function MenuBar() {
 
     const classes = useStyles();
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
     return (
         <AppBar position="static">
@@ -40,26 +45,32 @@ function MenuBar() {
                 <Typography variant="h4" className={classes.logo}>
                     Resterant Management System
                 </Typography>
-                <div className={classes.navlinks}>
-                    <Link to="/" className={classes.link}>
-                        Home
-                    </Link>
-                    <Link to="/diningtables" className={classes.link}>
-                        Dining Tables
-                    </Link>
-                    <Link to="/customers" className={classes.link}>
-                        Customers
-                    </Link>
-                    <Link to="/Waitlist" className={classes.link}>
-                        Waitlist
-                    </Link>
-                    <Link to="/Waiters" className={classes.link}>
-                        Waiters
-                    </Link>
-                    <Link to="/Visits" className={classes.link}>
-                        Visits
-                    </Link>
-                </div>
+                {isMobile ? (
+                    <DrawerComponent />
+                ) : (
+
+
+                    <div className={classes.navlinks}>
+                        <Link to="/" className={classes.link}>
+                            Home
+                        </Link>
+                        <Link to="/diningtables" className={classes.link}>
+                            Dining Tables
+                        </Link>
+                        <Link to="/customers" className={classes.link}>
+                            Customers
+                        </Link>
+                        <Link to="/Waitlist" className={classes.link}>
+                            Waitlist
+                        </Link>
+                        <Link to="/Waiters" className={classes.link}>
+                            Waiters
+                        </Link>
+                        <Link to="/Visits" className={classes.link}>
+                            Visits
+                        </Link>
+                    </div>
+                )}
             </Toolbar>
         </AppBar>
     );
