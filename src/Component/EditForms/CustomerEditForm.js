d56f53a5750e13c62f45186ee290f942ae6e7bb8
Charlie <cleungtech@outlook.com>
2021-12-06 14:04:48 -0500

    Implemented Edit Form for Customer

--- src/Component/EditForms/CustomerEditForm.js
+++ src/Component/EditForms/CustomerEditForm.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TriggerButton from './TriggerButton.js';
+import ReactDOM from 'react-dom';
+import FocusTrap from 'focus-trap-react';
 
-export const CustomerEditForm = ({ onSubmit }) => {
+const EditForm = ({ onSubmit }) => {
   return (
     <form onSubmit={onSubmit}>
       <div className="form-group">
@@ -24,4 +27,95 @@ export const CustomerEditForm = ({ onSubmit }) => {
     </form>
   );
 };
-export default CustomerEditForm;
+
+const CustomerModal = ({
+  onClickOutside,
+  onKeyDown,
+  modalRef,
+  buttonRef,
+  closeModal,
+  onSubmit
+}) => {
+  return ReactDOM.createPortal(
+    <FocusTrap>
+      <aside
+        tag="aside"
+        role="dialog"
+        tabIndex="-1"
+        aria-modal="true"
+        className="modal-cover"
+        onClick={onClickOutside}
+        onKeyDown={onKeyDown}
+      >
+        <div className="modal-area" ref={modalRef}>
+          <button
+            ref={buttonRef}
+            aria-label="Close Modal"
+            aria-labelledby="close-modal"
+            className="_modal-close"
+            onClick={closeModal}
+          >
+            <span id="close-modal" className="_hide-visual">
+              Close
+            </span>
+            <svg className="_modal-close-icon" viewBox="0 0 40 40">
+              <path d="M 10,10 L 30,30 M 30,10 L 10,30" />
+            </svg>
+          </button>
+          <div className="modal-body">
+            <EditForm onSubmit={onSubmit} />
+          </div>
+        </div>
+      </aside>
+    </FocusTrap>,
+    document.body
+  );
+};
+
+export class CustomerEditForm extends Component {
+  state = { isShown: false };
+  showModal = () => {
+    this.setState({ isShown: true }, () => {
+      this.closeButton.focus();
+    });
+    this.toggleScrollLock();
+  };
+  closeModal = () => {
+    this.setState({ isShown: false });
+    this.TriggerButton.focus();
+    this.toggleScrollLock();
+  };
+  onKeyDown = (event) => {
+    if (event.keyCode === 27) {
+      this.closeModal();
+    }
+  };
+  onClickOutside = (event) => {
+    if (this.modal && this.modal.contains(event.target)) return;
+    this.closeModal();
+  };
+
+  toggleScrollLock = () => {
+    document.querySelector('html').classList.toggle('scroll-lock');
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <TriggerButton
+          showModal={this.showModal}
+          buttonRef={(n) => (this.TriggerButton = n)}
+        />
+        {this.state.isShown ? (
+          <CustomerModal
+            onSubmit={this.props.onSubmit}
+            modalRef={(n) => (this.modal = n)}
+            buttonRef={(n) => (this.closeButton = n)}
+            closeModal={this.closeModal}
+            onKeyDown={this.onKeyDown}
+            onClickOutside={this.onClickOutside}
+          />
+        ) : null}
+      </React.Fragment>
+    );
+  }
+}
